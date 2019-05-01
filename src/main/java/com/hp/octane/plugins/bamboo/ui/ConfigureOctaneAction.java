6d487dbb5a482322ac20edf7adb378d0cb2699eb
elishevaHa <elish654@gmail.com>
2019-05-01 10:48:24 +0300

    defect #832119: client secret is exposed in configuration in plain text

--- src/main/java/com/hp/octane/plugins/bamboo/ui/ConfigureOctaneAction.java
+++ src/main/java/com/hp/octane/plugins/bamboo/ui/ConfigureOctaneAction.java
@@ -29,117 +29,125 @@ import org.slf4j.LoggerFactory;
 import java.util.UUID;
 
 public class ConfigureOctaneAction extends BambooActionSupport {
-	private static final Logger logger = LoggerFactory.getLogger(ConfigureOctaneAction.class);
+    private static final Logger logger = LoggerFactory.getLogger(ConfigureOctaneAction.class);
 
-	private final PluginSettingsFactory settingsFactory;
+    private static PluginSettingsFactory settingsFactory;
 
-	private String octaneUrl;
-	private String accessKey;
-	private String apiSecret;
-	private String userName;
-	private String uuid;
+    public static final String PLAIN_PASSWORD = "_plain_password_";
+    private String octaneUrl;
+    private String accessKey;
+    private String apiSecret;
+    private String userName;
+    private String uuid;
 
-	public ConfigureOctaneAction(PluginSettingsFactory settingsFactory, BambooPermissionManager bambooPermissionManager) {
-	    this.setBambooPermissionManager(bambooPermissionManager);
-		this.settingsFactory = settingsFactory;
-		readData();
-	}
+    public ConfigureOctaneAction(PluginSettingsFactory settingsFactory, BambooPermissionManager bambooPermissionManager) {
+        this.setBambooPermissionManager(bambooPermissionManager);
+        this.settingsFactory = settingsFactory;
+        readData();
+    }
 
-	public String doEdit() {
+    public String doEdit() {
         logger.info("edit configuration");
-        if(!this.hasAdminPermission()){
+        if (!this.hasAdminPermission()) {
             logger.error("Access Denied, no admin permissions.");
             throw new AccessDeniedException(null);
         }
-		return INPUT;
-	}
+        return INPUT;
+    }
 
-	public String doSave() {
-		logger.info("save configuration");
-        if(!this.hasAdminPermission()){
+    public String doSave() {
+        logger.info("save configuration");
+        if (!this.hasAdminPermission()) {
             logger.error("Access Denied, no admin permissions.");
             throw new AccessDeniedException(null);
         }
 
-		PluginSettings settings = settingsFactory.createGlobalSettings();
-		settings.put(OctaneConfigurationKeys.OCTANE_URL, octaneUrl);
-		settings.put(OctaneConfigurationKeys.ACCESS_KEY, accessKey);
-		settings.put(OctaneConfigurationKeys.API_SECRET, apiSecret);
-		settings.put(OctaneConfigurationKeys.IMPERSONATION_USER, userName);
-		addActionMessage("Configuration updated successfully");
-		//todo should be changed for multi shared space
-		Utils.cud(octaneUrl, uuid, accessKey, apiSecret);
-		return SUCCESS;
-	}
+        PluginSettings settings = settingsFactory.createGlobalSettings();
+        settings.put(OctaneConfigurationKeys.OCTANE_URL, octaneUrl);
+        settings.put(OctaneConfigurationKeys.ACCESS_KEY, accessKey);
+        if (!PLAIN_PASSWORD.equals(apiSecret)) {
+            settings.put(OctaneConfigurationKeys.API_SECRET, apiSecret);
+        }
+        settings.put(OctaneConfigurationKeys.IMPERSONATION_USER, userName);
+        addActionMessage("Configuration updated successfully");
+        //todo should be changed for multi shared space
+        Utils.cud(octaneUrl, uuid, accessKey, apiSecret);
+        return SUCCESS;
+    }
 
-	public String getUserName() {
-		return userName;
-	}
+    public static String readApiSecretFromSettings() {
+        PluginSettings settings = settingsFactory.createGlobalSettings();
+        return String.valueOf(settings.get(OctaneConfigurationKeys.API_SECRET));
+    }
 
-	public void setUserName(String username) {
-		userName = username;
-	}
+    public String getUserName() {
+        return userName;
+    }
 
-	public String getOctaneUrl() {
-		return octaneUrl;
-	}
+    public void setUserName(String username) {
+        userName = username;
+    }
 
-	public void setOctaneUrl(String octaneUrl) {
-		this.octaneUrl = octaneUrl;
-	}
+    public String getOctaneUrl() {
+        return octaneUrl;
+    }
 
-	public String getAccessKey() {
-		return accessKey;
-	}
+    public void setOctaneUrl(String octaneUrl) {
+        this.octaneUrl = octaneUrl;
+    }
 
-	public void setAccessKey(String accessKey) {
-		this.accessKey = accessKey;
-	}
+    public String getAccessKey() {
+        return accessKey;
+    }
 
-	public String getApiSecret() {
-		return apiSecret;
-	}
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
 
-	public void setApiSecret(String apiSecret) {
-		this.apiSecret = apiSecret;
-	}
+    public String getApiSecret() {
+        return PLAIN_PASSWORD;//always return plain_password  constant in order to not expose the real password to the client
+    }
 
-	public String getUuid() {
-		return uuid;
-	}
+    public void setApiSecret(String apiSecret) {
+        this.apiSecret = apiSecret;
+    }
 
-	public void setUuid(String uuid) {
-		this.uuid = uuid;
-	}
+    public String getUuid() {
+        return uuid;
+    }
 
-	private void readData() {
-		PluginSettings settings = settingsFactory.createGlobalSettings();
-		if (settings.get(OctaneConfigurationKeys.UUID) != null) {
-			uuid = String.valueOf(settings.get(OctaneConfigurationKeys.UUID));
-		} else {
-			// generate new UUID
-			uuid = UUID.randomUUID().toString();
-			settings.put(OctaneConfigurationKeys.UUID, uuid);
-		}
-		if (settings.get(OctaneConfigurationKeys.OCTANE_URL) != null) {
-			octaneUrl = String.valueOf(settings.get(OctaneConfigurationKeys.OCTANE_URL));
-		} else {
-			octaneUrl = "";
-		}
-		if (settings.get(OctaneConfigurationKeys.ACCESS_KEY) != null) {
-			accessKey = String.valueOf(settings.get(OctaneConfigurationKeys.ACCESS_KEY));
-		} else {
-			accessKey = "";
-		}
-		if (settings.get(OctaneConfigurationKeys.API_SECRET) != null) {
-			apiSecret = String.valueOf(settings.get(OctaneConfigurationKeys.API_SECRET));
-		} else {
-			apiSecret = "";
-		}
-		if (settings.get(OctaneConfigurationKeys.IMPERSONATION_USER) != null) {
-			userName = String.valueOf(settings.get(OctaneConfigurationKeys.IMPERSONATION_USER));
-		} else {
-			userName = "";
-		}
-	}
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    private void readData() {
+        PluginSettings settings = settingsFactory.createGlobalSettings();
+        if (settings.get(OctaneConfigurationKeys.UUID) != null) {
+            uuid = String.valueOf(settings.get(OctaneConfigurationKeys.UUID));
+        } else {
+            // generate new UUID
+            uuid = UUID.randomUUID().toString();
+            settings.put(OctaneConfigurationKeys.UUID, uuid);
+        }
+        if (settings.get(OctaneConfigurationKeys.OCTANE_URL) != null) {
+            octaneUrl = String.valueOf(settings.get(OctaneConfigurationKeys.OCTANE_URL));
+        } else {
+            octaneUrl = "";
+        }
+        if (settings.get(OctaneConfigurationKeys.ACCESS_KEY) != null) {
+            accessKey = String.valueOf(settings.get(OctaneConfigurationKeys.ACCESS_KEY));
+        } else {
+            accessKey = "";
+        }
+        if (settings.get(OctaneConfigurationKeys.API_SECRET) != null) {
+            apiSecret = String.valueOf(settings.get(OctaneConfigurationKeys.API_SECRET));
+        } else {
+            apiSecret = "";
+        }
+        if (settings.get(OctaneConfigurationKeys.IMPERSONATION_USER) != null) {
+            userName = String.valueOf(settings.get(OctaneConfigurationKeys.IMPERSONATION_USER));
+        } else {
+            userName = "";
+        }
+    }
 }
