6f28bde62a492dcadab7d84a716183ac42787a62
Radislav Berkovich <radislav.berkovich@microfocus.com>
2019-08-07 11:38:56 +0300

    tech : fixing start in agent

--- src/main/java/com/hp/octane/plugins/bamboo/admin/AdminServlet.java
+++ src/main/java/com/hp/octane/plugins/bamboo/admin/AdminServlet.java
@@ -19,11 +19,12 @@ package com.hp.octane.plugins.bamboo.admin;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.sal.api.auth.LoginUriProvider;
+import com.atlassian.sal.api.component.ComponentLocator;
 import com.atlassian.sal.api.user.UserManager;
+import com.atlassian.sal.api.user.UserProfile;
 import com.atlassian.templaterenderer.TemplateRenderer;
 
 import javax.inject.Inject;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,24 +33,21 @@ import java.net.URI;
 
 @Scanned
 public class AdminServlet extends HttpServlet {
-    @ComponentImport
-    private final UserManager userManager;
-    @ComponentImport
-    private final LoginUriProvider loginUriProvider;
+
     @ComponentImport
     private final TemplateRenderer renderer;
 
     @Inject
-    public AdminServlet(UserManager userManager, LoginUriProvider loginUriProvider, TemplateRenderer renderer) {
-        this.userManager = userManager;
-        this.loginUriProvider = loginUriProvider;
+    public AdminServlet(TemplateRenderer renderer) {
         this.renderer = renderer;
     }
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String username = userManager.getRemoteUsername(request);
-        if (username == null || !userManager.isSystemAdmin(username)) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        UserManager userManager = ComponentLocator.getComponent(UserManager.class);
+
+        UserProfile username = userManager.getRemoteUser(request);
+        if (username == null || !userManager.isSystemAdmin(username.getUserKey())) {
             redirectToLogin(request, response);
             return;
         }
@@ -59,6 +57,7 @@ public class AdminServlet extends HttpServlet {
     }
 
     private void redirectToLogin(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        LoginUriProvider loginUriProvider = ComponentLocator.getComponent(LoginUriProvider.class);
         response.sendRedirect(loginUriProvider.getLoginUri(getUri(request)).toASCIIString());
     }
 
