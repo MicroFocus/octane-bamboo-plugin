f19b5786343784adbfaf760df643da97d17476b6
Radislav Berkovich <radislav.berkovich@microfocus.com>
2020-08-24 16:49:23 +0300

    defect #1064349 : [Bamboo] Bamboo plugin doesn't support job move

--- src/main/java/com/hp/octane/plugins/bamboo/listener/GeneralEventsListener.java
+++ src/main/java/com/hp/octane/plugins/bamboo/listener/GeneralEventsListener.java
@@ -16,22 +16,33 @@
 
 package com.hp.octane.plugins.bamboo.listener;
 
+import com.atlassian.bamboo.chains.events.ChainMovedEvent;
 import com.atlassian.bamboo.event.ChainDeletedEvent;
 import com.atlassian.bamboo.event.HibernateEventListenerAspect;
+import com.atlassian.bamboo.plan.cache.CachedPlanManager;
+import com.atlassian.bamboo.plan.cache.ImmutableChain;
 import com.atlassian.bamboo.v2.build.events.PostBuildCompletedEvent;
 import com.atlassian.event.api.EventListener;
 import com.atlassian.plugin.event.events.PluginDisablingEvent;
 import com.atlassian.plugin.event.events.PluginEnabledEvent;
+import com.atlassian.sal.api.component.ComponentLocator;
 import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
+import com.hp.octane.integrations.OctaneSDK;
+import com.hp.octane.integrations.dto.DTOFactory;
+import com.hp.octane.integrations.dto.events.CIEvent;
+import com.hp.octane.integrations.dto.events.CIEventType;
+import com.hp.octane.integrations.dto.events.ItemType;
 import com.hp.octane.plugins.bamboo.octane.BambooPluginServices;
 import com.hp.octane.plugins.bamboo.rest.OctaneConnectionManager;
 
 public class GeneralEventsListener extends BaseListener {
 
     private PluginSettingsFactory settingsFactory;
+    private CachedPlanManager planMan;
 
     public GeneralEventsListener(PluginSettingsFactory settingsFactory) {
         this.settingsFactory = settingsFactory;
+        this.planMan = ComponentLocator.getComponent(CachedPlanManager.class);
     }
 
     @EventListener
@@ -40,6 +51,39 @@ public class GeneralEventsListener extends BaseListener {
             return;
         }
         MultibranchHelper.onChainDeleted(event.getPlanKey());
+    }
+
+    @EventListener
+    public void onChainMoved(ChainMovedEvent event) {
+        if (!OctaneConnectionManager.hasActiveClients()) {
+            return;
+        }
+        log.info("onChainMoved " + event.getOriginalPlanKey() + "=>" + event.getNewPlanKey());
+        CIEvent renamedEvent = DTOFactory.getInstance().newDTO(CIEvent.class).setEventType(CIEventType.RENAMED);
+        ImmutableChain chain = (ImmutableChain) planMan.getPlanByKey(event.getNewPlanKey());
+        if (MultibranchHelper.isMultiBranchParent(chain)) {
+            renamedEvent.setItemType(ItemType.MULTI_BRANCH);
+        } else {
+            renamedEvent.setItemType(ItemType.JOB);
+        }
+
+        renamedEvent.setProject(event.getNewPlanKey().getKey())
+                .setProjectDisplayName(chain.getName())
+                .setPreviousProject(event.getOriginalPlanKey().getKey())
+                .setPreviousProjectDisplayName(chain.getName());
+
+        //second event to update inner plans
+        CIEvent renamedEvent2 =  DTOFactory.getInstance().newDTO(CIEvent.class).setEventType(CIEventType.RENAMED)
+                .setItemType(ItemType.FOLDER)
+                .setProject(event.getNewPlanKey().getKey())
+                .setProjectDisplayName(chain.getName())
+                .setPreviousProject(event.getOriginalPlanKey().getKey())
+                .setPreviousProjectDisplayName(chain.getName());
+
+        OctaneSDK.getClients().forEach(client -> client.getEventsService().publishEvent(renamedEvent));
+        OctaneSDK.getClients().forEach(client -> client.getEventsService().publishEvent(renamedEvent2));
+
+
     }
 
     @EventListener
